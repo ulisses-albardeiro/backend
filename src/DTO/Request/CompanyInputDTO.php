7f154ceba170963dcb8aa3f286682cafdf717f5d
Ulisses <ualbardeiro@gmail.com>
2026-02-07 15:21:09 -0300

    feate:create DTO with Symfony Validator

---
+++ src/DTO/Request/CompanyInputDTO.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\DTO\Request;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+readonly class CompanyInputDTO
+{
+    public function __construct(
+        #[Assert\Length(max: 255)]
+        public ?string $name,
+
+        #[Assert\NotBlank]
+        #[Assert\Length(max: 255)]
+        public string $tradingName,
+
+        #[Assert\Length(min: 14, max: 18)]
+        public ?string $registrationNumber,
+
+        #[Assert\NotBlank]
+        #[Assert\Length(max: 50)]
+        public string $stateRegistration,
+
+        #[Assert\Email]
+        #[Assert\Length(max: 50)]
+        public ?string $email,
+
+        #[Assert\Length(max: 15)]
+        public ?string $phone,
+
+        public ?string $website,
+
+        #[Assert\Length(max: 10)]
+        public ?string $zipCode,
+
+        public ?string $street,
+        public ?string $number,
+        public ?string $complement,
+        public ?string $neighborhood,
+
+        #[Assert\NotBlank]
+        public string $city,
+
+        #[Assert\NotBlank]
+        #[Assert\Length(max: 50)]
+        public string $state,
+
+        public ?string $logo = null,
+    ) {}
+}
---
+++ src/DTO/Response/CompanyOutputDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DTO\Response;
+
+readonly class CompanyOutputDTO
+{
+    public function __construct(
+        public int $id,
+        public ?string $name,
+        public string $tradingName,
+        public ?string $registrationNumber,
+        public string $stateRegistration,
+        public ?string $email,
+        public ?string $phone,
+        public ?string $logo,
+        public ?string $website,
+        public ?string $zipCode,
+        public ?string $street,
+        public ?string $number,
+        public ?string $complement,
+        public ?string $neighborhood,
+        public string $city,
+        public string $state,
+    ) {}
+}
