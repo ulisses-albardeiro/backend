a97b1dd07aa3726a0f85b2f010c4b0f8810b9f8b
Ulisses <ualbardeiro@gmail.com>
2026-02-13 10:37:58 -0300

    Adjusts input DTO in Transactions

--- src/DTO/Request/TransactionInputDTO.php
+++ src/DTO/Request/TransactionInputDTO.php
@@ -2,8 +2,6 @@
 
 namespace App\DTO\Request;
 
-use App\Enum\TransactionStatus;
-use App\Enum\TransactionType;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class TransactionInputDTO
@@ -22,11 +20,9 @@ class TransactionInputDTO
     public string $date;
 
     #[Assert\NotBlank]
-    #[Assert\Choice(callback: [TransactionType::class, 'values'])]
     public string $type;
 
     #[Assert\NotBlank]
-    #[Assert\Choice(callback: [TransactionStatus::class, 'values'])]
     public string $status;
 
     #[Assert\NotBlank]
