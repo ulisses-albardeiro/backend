22cf8e4f6a8db3d048281b50eaeb863ddb40c48f
Ulisses <ualbardeiro@gmail.com>
2026-05-02 15:55:21 -0300

    Adiciona campos de created e updated na entity LaborCategory

--- src/Entity/Labor/LaborCategory.php
+++ src/Entity/Labor/LaborCategory.php
@@ -42,6 +42,12 @@ class LaborCategory
     #[ORM\Column(enumType: LaborCategoryStatus::class)]
     private ?LaborCategoryStatus $status = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
     public function __construct()
     {
         $this->subCategories = new ArrayCollection();
@@ -150,6 +156,30 @@ class LaborCategory
     public function setStatus(LaborCategoryStatus $status): static
     {
         $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
