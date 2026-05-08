f2b9fe6364cff1c1bf59508c167caae3ca50c360
Ulisses <ualbardeiro@gmail.com>
2026-05-08 12:13:39 -0300

    Adiciona company, createdAr e updatedAt em CustomerAsset

--- src/Entity/Customer/CustomerAsset.php
+++ src/Entity/Customer/CustomerAsset.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity\Customer;
 
+use App\Entity\Company;
 use App\Repository\Customer\CustomerAssetRepository;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -19,6 +20,16 @@ class CustomerAsset
     #[ORM\ManyToOne(inversedBy: 'customerAssets')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Customer $customer = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Company $company = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
 
     public function getId(): ?int
     {
@@ -45,6 +56,42 @@ class CustomerAsset
     public function setCustomer(?Customer $customer): static
     {
         $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getCompany(): ?Company
+    {
+        return $this->company;
+    }
+
+    public function setCompany(?Company $company): static
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
